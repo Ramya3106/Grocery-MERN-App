a774a97b27c179fb333849f93c039249fd4f88f6
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-08-21 22:58:49 +0530

    updated at app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes,Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import ProductDetails from "./pages/ProductDetails";
+import Cart from "./pages/Cart";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element = {<Home/>}/>
         <Route path="/products" element = {<Products/>}/>
         <Route path="/products/:id" element = {<ProductDetails/>}/>
+        <Route path="/Cart" element = {<Cart/>}/>
       </Routes>
       </div>
     </div>
