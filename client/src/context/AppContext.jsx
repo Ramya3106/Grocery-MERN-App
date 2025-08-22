9c014f01e681fa02561ac56c91d770844f3ce641
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-08-22 21:07:52 +0530

    updated at appcontext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,6 +1,5 @@
 import { createContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
 export const AppContext = createContext(null);
 
 const AppContextProvider = ({ children }) => {
