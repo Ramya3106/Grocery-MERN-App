6a3c8e3764f9673aa2ffb4974cbe5718a177cb8d
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-08-22 21:02:12 +0530

    updated at appcontext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -6,6 +6,8 @@ export const AppContext = createContext(null);
 const AppContextProvider = ({ children }) => {
     const navigate = useNavigate();
     const [user, setuser ] = useState(null);
+    const [isSeller, setSeller] = useState(null);
+    const [showUserLogin, setShowUserLogin] = useState(false);
     const value = {navigate}
     return <AppContextProvider value={value}>{children}</AppContextProvider>
 }
