e9db0b47bb875c6146a629d84ba856073c5059b7
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-08-23 19:04:13 +0530

    updated at navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-const App = () => {
+const Navbar = () => {
     const [open, setOpen] = React.useState(false)
     return (
         <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
