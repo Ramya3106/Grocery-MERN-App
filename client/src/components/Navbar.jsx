9672c0cd82047437651b5a031f625b348e947fcf
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-08-23 19:09:37 +0530

    updated at navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     const [open, setOpen] = useState(false)
     return (
         <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
 
-            
+            <Link>
+            <h1>Grocery App</h1></Link>
 
             {/* Desktop Menu */}
             <div className="hidden sm:flex items-center gap-8">
