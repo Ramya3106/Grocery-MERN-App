60b6c7ee6f0b43d76b949a50de639f509894cc53
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-08-24 10:50:23 +0530

    updated at navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -36,8 +36,8 @@ const Navbar = () => {
                    <div className="relative group">
                     <img src={assets.profile_icon} alt="" className="w-10"/>
                     <ul className="hidden group-hover:block absolute top-10 right-0 bg-white shadow-md rounded-md borer border-gray-200 py-2 w-30 z-40 text-sm">
-                        <li>My Orders</li>
-                        <li>Logout</li>
+                        <li className="p-1.5 cursor-pointer">My Orders</li>
+                        <li className="p-1.5 curdor-pointer">Logout</li>
                     </ul>
                     </div>
                     </>
