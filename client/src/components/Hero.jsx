ec9a1c85fc98be69534c732d11aaf2fc18f7a398
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-08-24 19:33:14 +0530

    updated at hero.jsx

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { assets } from "../assets/assets";
 const Hero = () => {
   return (
     <div className="relative">
-        <img src={assets.main_banner_bg} alt="" />
+        <img src={assets.main_banner_bg} alt="" className="hidden md:block w-full" />
     </div>
   )
 }
