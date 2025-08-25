23d7f53bbca5add2d94faf09183d3b1b5d8bb2a8
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-08-25 19:17:29 +0530

    updated at hero.jsx

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -4,6 +4,9 @@ const Hero = () => {
     <div className="relative">
         <img src={assets.main_banner_bg} alt="" className="hidden md:block w-full" />
         <img src={assets.main_banner_bg_sm} alt="" className="md:hidden w-full" />
+        <div className="absolute inset-0 flex flex-col items-center">
+
+        </div>
     </div>
   )
 }
