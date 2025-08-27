a2dd15c19d3bf2f4856dd72a5f66be765558cc4c
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-08-27 21:36:06 +0530

    updated at category.jsx

--- client/src/components/Category.jsx
+++ client/src/components/Category.jsx
@@ -2,6 +2,11 @@ const Category = () => {
   return (
     <div className="mt-16">
         <p className="text-2xl font-medium md:text-3xl">Category</p>
+        <div>
+            {
+                
+            }
+        </div>
     </div>
   )
 }
