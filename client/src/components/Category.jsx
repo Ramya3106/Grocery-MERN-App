fa3be0fb7d425c572c33b0c50fe1754424cf64ae
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-08-27 21:50:01 +0530

    updated at category.jsx

--- client/src/components/Category.jsx
+++ client/src/components/Category.jsx
@@ -1,10 +1,12 @@
+import { categories } from "../assets/assets"
+
 const Category = () => {
   return (
     <div className="mt-16">
         <p className="text-2xl font-medium md:text-3xl">Category</p>
         <div>
             {
-                Category.map((category,index)=>(
+                categories.map((category,index)=>(
                     <div key={index} className={`group cursor-pointer py-5 px-3 rounded-lg gap-2 flex flex-col items-center justify-center`}>
                         <img src={category.img} alt="" className="max-w-28 transition group-hover:scale-110" />
                         <p className="text-sm font-medium">{category.text}</p>
