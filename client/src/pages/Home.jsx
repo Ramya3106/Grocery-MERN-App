d3e1ee81027b7956dc27f0c5ac9103e139186e00
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-08-27 21:32:19 +0530

    updated at home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import Category from '../components/Category'
 import Hero from '../components/Hero'
 
 const Home = () => {
   return (
     <div className='mt-10'>
       <Hero/>
+      <Category />
     </div>
   )
 }
