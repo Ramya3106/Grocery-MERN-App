471df917969faf49f402c633e7103f4271050829
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-08-28 22:20:03 +0530

    updated at category.jsx

--- client/src/components/Category.jsx
+++ client/src/components/Category.jsx
@@ -1,21 +1,28 @@
 import { categories } from "../assets/assets"
 
 const Category = () => {
-  return (
-    <div className="mt-16">
-        <p className="text-2xl font-medium md:text-3xl">Category</p>
-        <div className="my-6 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 md:grid-cols-7 gap-3 items-center justify-center">
-            {
-                categories.map((category,index)=>(
-                    <div key={index} className={`group cursor-pointer py-5 px-3 rounded-lg gap-2 flex flex-col items-center justify-center`}>
-                        <img src={category.image} alt="" className="max-w-28 transition group-hover:scale-110" />
-                        <p className="text-sm font-medium">{category.text}</p>
-                    </div>
-                ))
-            }
-        </div>
-    </div>
-  )
+        return (
+            <div className="mt-16">
+                <p className="text-2xl font-medium md:text-3xl">Category</p>
+                <div className="my-6 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 md:grid-cols-7 gap-3 items-center justify-center">
+                    {categories.map((category, index) => (
+                        <div
+                            key={index}
+                            className="group cursor-pointer py-5 px-3 rounded-lg gap-2 flex flex-col items-center justify-center"
+                             style={{ backgroundColor: category.bgColor }}
+                        >
+                            <img
+                                src={category.image}
+                                alt=""
+                                className="max-w-20 mb-2 rounded-lg bg-white p-2"
+                                style={{ background: 'transparent' }}
+                            />
+                            <p className="text-sm font-medium mt-2">{category.text}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        );
 }
 
 export default Category
