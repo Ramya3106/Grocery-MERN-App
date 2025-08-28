c873017407837bc975a04cdeb033283e34b5cbad
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-08-28 23:40:42 +0530

    updated at productcard.jsx

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -3,11 +3,12 @@ import { AppContext } from "../context/AppContext";
 
 const ProductCard = ({products}) => {
     const ProductCard = ({ product }) => {
-        const { navigate } = useContent(AppContent);
+        const { navigate } = useContext(AppContext);
     }
   return product && (
     <div className="border bordeer-gray-500/20 rounded-md md:px-4 px-3 py-2
     bg-white min-w-56 max-w-56 w-full">
+        
 
     </div>
   )
