32cc04d74c5fd03b0d4357a9b36934c4196ad396
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-08-28 23:02:26 +0530

    updated at bestseller.jsx

--- client/src/components/BestSeller.jsx
+++ client/src/components/BestSeller.jsx
@@ -1,7 +1,7 @@
 const BestSeller = () => {
   return (
     <div className="mt-16">
-        <p className="text-2xl font-medium md:text-3xl">Category</p>
+        <p className="text-2xl font-medium md:text-3xl">Best Sellers</p>
     </div>
   )
 }
