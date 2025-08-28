1f1ef7e250aa4a1269b36f1e0ecfd28bc5046736
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-08-28 22:21:08 +0530

    updated at category.jsx

--- client/src/components/Category.jsx
+++ client/src/components/Category.jsx
@@ -6,7 +6,9 @@ const Category = () => {
                 <p className="text-2xl font-medium md:text-3xl">Category</p>
                 <div className="my-6 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 md:grid-cols-7 gap-3 items-center justify-center">
                     {categories.map((category, index) => (
-                        <div
+                        <div onClick={() => {
+                            
+                        }}
                             key={index}
                             className="group cursor-pointer py-5 px-3 rounded-lg gap-2 flex flex-col items-center justify-center"
                              style={{ backgroundColor: category.bgColor }}
