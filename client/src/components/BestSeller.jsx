5b328b1a2d7f50585ba12b0a1299cda8ab8b4b5b
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-08-28 23:15:37 +0530

    updated at appcontext.jsx

--- client/src/components/BestSeller.jsx
+++ client/src/components/BestSeller.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react"
 import { AppContext } from "../context/AppContext"
 const BestSeller = () => {
+    const { Products } = useContext(AppContext);
   return (
     <div className="mt-16">
         <p className="text-2xl font-medium md:text-3xl">Best Sellers</p>
