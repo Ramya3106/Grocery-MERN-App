403a767486867a13a8d35411e772e06742818da4
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-08-28 23:39:10 +0530

    updated at productcard.jsx

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -7,7 +7,7 @@ const ProductCard = ({products}) => {
     }
   return product && (
     <div className="border bordeer-gray-500/20 rounded-md md:px-4 px-3 py-2
-    bg-white  min-w-56">
+    bg-white min-w-56 max-w-56 w-full">
 
     </div>
   )
