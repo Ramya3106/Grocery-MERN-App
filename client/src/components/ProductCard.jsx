1a606a56cf05a46c63c8ffae6bcb9c58aa4aa145
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-08-28 23:36:57 +0530

    updated at productcard.jsx

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -6,7 +6,7 @@ const ProductCard = ({products}) => {
         const { navigate } = useContent(AppContent);
     }
   return product && (
-    <div className="border bordeer-gray-500/20">
+    <div className="border bordeer-gray-500/20 rounded-md">
 
     </div>
   )
