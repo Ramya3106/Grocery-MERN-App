0dc4c2c0078645b80cb85f8829f406ebb5d9d103
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-09-01 14:36:14 +0530

    updated at productcard.jsx

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -8,6 +8,9 @@ const ProductCard = ({products}) => {
   return product && (
     <div className="border bordeer-gray-500/20 rounded-md md:px-4 px-3 py-2
     bg-white min-w-56 max-w-56 w-full">
+      <div  className="group cursor-poiter flex items-center justify-center">
+
+        </div>
         
 
     </div>
