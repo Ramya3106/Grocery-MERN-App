724c5e414fa1c450944fe7a6e40c05c2eae0db56
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-09-01 14:40:43 +0530

    updated at productcard.jsx

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -9,7 +9,7 @@ const ProductCard = ({products}) => {
     <div className="border bordeer-gray-500/20 rounded-md md:px-4 px-3 py-2
     bg-white min-w-56 max-w-56 w-full">
       <div  className="group cursor-poiter flex items-center justify-center px-2">
-        <img src="" alt=""/>
+        <img src={product.image[0]} alt=""/>
 
         </div>
         
