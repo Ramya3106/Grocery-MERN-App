a0031d41e6309d507f38da383a86fa6eff97cefa
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-09-08 20:11:08 +0530

    updated at newsletter.jsx

--- client/src/components/NewsLetter.jsx
+++ client/src/components/NewsLetter.jsx
@@ -1,6 +1,6 @@
 const NewsLetter = () => {
   return (
-    <div className="flex flex-col items-center justify-center text-center space-y-2">
+    <div className="py-16 flex flex-col items-center justify-center text-center space-y-2">
             <h1 className="md:text-4xl text-2xl font-semibold">Never Miss a Deal!</h1>
             <p className="md:text-lg text-gray-500/70 pb-8">
                 Subscribe to get the latest offers, new arrivals, and exclusive discounts
