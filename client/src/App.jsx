37054dd4233b71b5d4c4ae7c781c8751a794156c
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-09-10 19:19:15 +0530

    updated at appcontext.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,6 +21,7 @@ const App = () => {
       {
         showUserLogin?<Auth/> :null
       }
+      <Toaster />
       <div className="px-6 md:px-16 lg:px-24 xl:px-32">
         <Routes>
         <Route path="/" element = {<Home/>}/>
