3439aae0f1c43989d38b94b5ffafcdc15c6ae193
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-09-19 19:06:05 +0530

    updated at productcard.jsx

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -3,7 +3,7 @@ import { AppContext } from "../context/AppContext";
 import { assets } from "../assets/assets";
 
 const ProductCard = ({product}) => {
-  const { navigate } = useContext(AppContext);  
+  const { navigate,addToCart,cartItems } = useContext(AppContext);  
   const [count, setCount] = useState(0);
     
   return (
