490376e05ecc374d5231f557d2f8b6d8dda78ba7
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-10-02 13:11:13 +0530

    refactor(ProductDetails): remove hardcoded product data for improved flexibility

--- client/src/pages/ProductDetails.jsx
+++ client/src/pages/ProductDetails.jsx
@@ -2,25 +2,7 @@ import { useState } from "react";
 
 const productDetails = () => {
 
-    const product = {
-        name: "Nike Pegasus 41 shoes",
-        category: "Sports",
-        price: 189,
-        offerPrice: 159,
-        rating: 4,
-        images: [
-            "https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/card/productImage.png",
-            "https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/card/productImage2.png",
-            "https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/card/productImage3.png",
-            "https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/card/productImage4.png"
-        ],
-        description: [
-            "High-quality material",
-            "Comfortable for everyday use",
-            "Available in different sizes"
-        ]
-    };
-
+    
     const [thumbnail, setThumbnail] = useState(product.images[0]);
 
     return product && (
