4abc536d4649e0a1a4cfa888612bb590b853bb6d
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-10-02 19:15:39 +0530

    updated at sellerlogin

--- client/src/components/seller/SellerLogin.jsx
+++ client/src/components/seller/SellerLogin.jsx
@@ -10,12 +10,7 @@ const SellerLogin = () => {
             <p className="text-2xl font-medium m-auto">
                 <span className="text-indigo-500">User</span> {state === "login" ? "Login" : "Sign Up"}
             </p>
-            {state === "register" && (
-                <div className="w-full">
-                    <p>Name</p>
-                    <input onChange={(e) => setName(e.target.value)} value={name} placeholder="type here" className="border border-gray-200 rounded w-full p-2 mt-1 outline-indigo-500" type="text" required />
-                </div>
-            )}
+            
             <div className="w-full ">
                 <p>Email</p>
                 <input onChange={(e) => setEmail(e.target.value)} value={email} placeholder="type here" className="border border-gray-200 rounded w-full p-2 mt-1 outline-indigo-500" type="email" required />
