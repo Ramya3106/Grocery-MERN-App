0cffefe8abf049a1a73b5c6b083d98c5c92e7a8b
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-10-02 19:35:14 +0530

    updated at sellerlogin

--- client/src/components/seller/SellerLogin.jsx
+++ client/src/components/seller/SellerLogin.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react'
+import React, { useContext,useEffect,useState } from 'react'
+import { AppContext } from '../../context/AppContext';
 
 const SellerLogin = () => {
   const {isSeller,setIsSeller,navigate}=useContext(AppContext)
