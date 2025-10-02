9fe198e461f388f664a5eca79b74bedeaf68156c
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-10-02 14:07:19 +0530

    updated at products

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -1,21 +1,21 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import ProductCard from "../components/ProductCard";
-import { useAppContext } from "../context/AppContext";
+import { AppContext } from "../context/AppContext";
 
 const Products = () => {
-  const { products, searchQuery } = useAppContext();
+  const { Products, searchQuery } = useContext(AppContext);
   const [filteredProducts, setFilteredProducts] = useState([]);
   useEffect(() => {
     if (searchQuery.length > 0) {
       setFilteredProducts(
-        products.filter((product) =>
+        Products.filter((product) =>
           product.name.toLowerCase().includes(searchQuery.toLowerCase())
         )
       );
     } else {
-      setFilteredProducts(products);
+      setFilteredProducts(Products);
     }
-  }, [products, searchQuery]);
+  }, [Products, searchQuery]);
   return (
     <div className="mt-16">
       <h1 className="text-3xl lg:text-4xl font-medium">All Products</h1>
