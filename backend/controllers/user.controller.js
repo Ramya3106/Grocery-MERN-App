6c1142e9a20c2e77d0a1d34451381087ae9765ef
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-10-07 21:15:45 +0530

    updated at usercontroller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -66,5 +66,11 @@ export const loginUser = async (req, res) => {
         .status(400)
         .json({ message: "User does not exist", success: false });
     }
+     const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ message: "Invalid credentials", success: false });
+    }
 
 
