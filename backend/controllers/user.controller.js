09f0eba0c334d8d065418ba1edbafacafdaad059
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-10-07 21:13:01 +0530

    updated at usercontroller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,14 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+
+// register user: /api/user/register
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Please fill all the fields", success: false });
+    }
+  
