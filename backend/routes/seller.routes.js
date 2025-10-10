7bb39c519ee56c57d57e8a5bbae0a53e60b6dcff
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-10-10 22:13:30 +0530

    updated at sellerroutes

--- backend/routes/seller.routes.js
+++ backend/routes/seller.routes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { sellerLogin,sellerLogout } from "../controllers/user.controller.js";
+import { isAuthSeller,sellerLogin,sellerLogout } from "../controllers/user.controller.js";
 import { authSeller } from "../middlewares/authSeller.js";
 const router = express.Router();
 
 router.post("/login", sellerLogin);
-router.get("/is-auth", authSeller, checkAuth);
+router.get("/is-auth", authSeller, isAuthSeller);
 router.get("/logout", authSeller, sellerLogout);
 
 export default router;
