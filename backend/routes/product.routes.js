46ce91b37e6f3518bf4b3b1ed3dc92f67ecb33ca
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-10-12 19:23:03 +0530

    updated at productroute

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { authSeller } from "../middlewares/authSeller.js";
+import { upload } from "../config/multer.js"
 const router = express.Router();
 
 router.post('/add-product',upload.array("images"));
