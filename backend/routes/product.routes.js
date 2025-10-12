731bf0538d12ea2d7ea82dabf2cc8a9253516837
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-10-12 19:12:48 +0530

    updated at productroute folder

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,5 +1,4 @@
 import express from "express";
-import { isAuthSeller,sellerLogin,sellerLogout } from "../controllers/user.controller.js";
 import { authSeller } from "../middlewares/authSeller.js";
 const router = express.Router();
 
