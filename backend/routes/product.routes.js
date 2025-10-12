37a7aef4557b2bc6d33107602776596efa7a4505
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-10-12 19:21:27 +0530

    updated at productroutes

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -2,6 +2,6 @@ import express from "express";
 import { authSeller } from "../middlewares/authSeller.js";
 const router = express.Router();
 
-router.post('/add-product',upload)
+router.post('/add-product',upload.array("images"));
 
 export default router;
