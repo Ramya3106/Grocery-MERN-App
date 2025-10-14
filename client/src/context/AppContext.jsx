1c060f7d2719cdb698a53c5558fc4adcd74e6325
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-10-14 11:16:07 +0530

    updated at appcontext

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { dummyProducts } from "../assets/assets";
 import toast from "react-hot-toast";
 import axios from "axios";
+axios.defaults.withCredentials = true;
 axios.defaults.baseURL=import.meta.env.VITE_BACKEND_URL;
 export const AppContext = createContext(null);
 
