67032f85443af876ebe9a29810a9c26debe49b2c
Ramya3106 <148340869+Ramya3106@users.noreply.github.com>
2025-10-17 18:22:59 +0530

    add route to retrieve all users for debugging purposes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -4,6 +4,7 @@ import {
   loginUser,
   logout,
   registerUser,
+  getAllUsers,
 } from "../controllers/user.controller.js";
 import authUser from "../middlewares/authUser.js";
 const router = express.Router();
@@ -12,5 +13,6 @@ router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/is-auth", authUser, checkAuth);
 router.get("/logout", authUser, logout);
+router.get("/all", getAllUsers); // Debug endpoint
 
 export default router;
